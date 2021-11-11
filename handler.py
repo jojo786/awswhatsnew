1c45cd072c48d7f8df1c23aaac0e7f3fa1ae463a
Yusuf Mayet <yusmayet@amazon.com>
2021-11-11 14:54:57 +0200

    Added filter for AWS Africa(Cape Town) region, and added integration to slack, changed region to af-south-1

--- handler.py
+++ handler.py
@@ -9,17 +9,27 @@ import twitter
 from botocore.client import Config
 from html.parser import HTMLParser
 from aws_lambda_powertools.utilities import parameters
-from TextToOwO import text_to_owo
+from slack_sdk import WebClient
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 ssm_provider = parameters.SSMProvider()
 
-consumer_key = ssm_provider.get("/cwoud/consumer_key", decrypt=True)
-consumer_secret = ssm_provider.get("/cwoud/consumer_secret", decrypt=True)
-access_token_key = ssm_provider.get("/cwoud/access_token_key", decrypt=True)
-access_token_secret = ssm_provider.get("/cwoud/access_token_secret", decrypt=True)
+consumer_key = ssm_provider.get("/whatsnew/twitter/consumer_key", decrypt=True)
+consumer_secret = ssm_provider.get("/whatsnew/twitter/consumer_secret", decrypt=True)
+access_token_key = ssm_provider.get("/whatsnew/twitter/access_token_key", decrypt=True)
+access_token_secret = ssm_provider.get("/whatsnew/twitter/access_token_secret", decrypt=True)
 
+api = twitter.Api(consumer_key=consumer_key,
+                  consumer_secret=consumer_secret,
+                  access_token_key=access_token_key,
+                  access_token_secret=access_token_secret)
+    
+
+bot_user_oauth_token = ssm_provider.get("/whatsnew/slack/consumer_key", decrypt=True)
+bot_channel_id = ssm_provider.get("/whatsnew/slack/channel_id", decrypt=True)
+
+client = WebClient(token=bot_user_oauth_token)
 
 class MLStripper(HTMLParser):
     def __init__(self):
@@ -41,16 +51,7 @@ def strip_tags(html):
     return s.get_data()
 
 
-
-api = twitter.Api(consumer_key=consumer_key,
-                  consumer_secret=consumer_secret,
-                  access_token_key=access_token_key,
-                  access_token_secret=access_token_secret)
-    
-
-
 posts_table = boto3.resource("dynamodb", region_name="us-west-2").Table(os.environ["PostsTableName"])
-
 
 def within(t: time.struct_time, minutes: int) -> bool:
     return abs(time.mktime(time.gmtime()) - time.mktime(t)) <= (minutes * 60)
@@ -59,30 +60,41 @@ def within(t: time.struct_time, minutes: int) -> bool:
 def already_posted(guid: str) -> bool:
     return "Item" in posts_table.get_item(Key={"guid": guid})
 
+def post_to_twitter(payload, entry):
+    api.PostUpdate(
+        payload
+        + "... "
+        + entry.link,
+        verify_status_length=False,
+    )
 
-def wambda_handwer(event, context):
-    recency_threshold = int(os.environ['PostRecencyThreshold'])
-    paywoad = ""
-    for entry in feedparser.parse("http://aws.amazon.com/new/feed/").entries:
-        #logger.info(f"Checking {entry.guid} - {entry.title}")
-        if within(entry.published_parsed, minutes=recency_threshold) and not already_posted(entry.guid):
-            logger.info(f"Posting {entry.guid} - {entry.title}")
-            paywoad = text_to_owo((entry.title + "\n\n" + strip_tags(entry.description))[:230])
-            try:
-                wength = 300
-                while len(paywoad) > 249:
-                    paywoad = text_to_owo((entry.title + "\n\n" + strip_tags(entry.description))[:230])
-                paywoad = paywoad.replace("ω\\", 'ω')
-                logger.info(f"Posting tweet with body length: " + str(len(paywoad)))
-                logger.info(f"Posting tweet with body: " + paywoad + "... " + entry.link)                
-                api.PostUpdate(
-                    paywoad
+def post_to_slack(payload, entry):
+    response = client.chat_postMessage(
+                    channel=bot_channel_id,
+                    text=payload
                     + "... "
-                    + entry.link,
-                    verify_status_length=False,
+                    + entry.link
                 )
-                posts_table.put_item(
-                    Item={"guid": entry.guid, "title": entry.title, "link": entry.link}
-                )
-            except Exception:
-                logger.exception(f"Faiwed to post tweet")
+
+def lambda_handler(event, context):
+    recency_threshold = int(os.environ['PostRecencyThreshold'])
+    payload = ""
+    for entry in feedparser.parse("http://aws.amazon.com/new/feed/").entries:
+        logger.info(f"Checking {entry.guid} - {entry.title}")
+        if within(entry.published_parsed, minutes=recency_threshold) and not already_posted(entry.guid):
+            if ("Cape Town" in entry.title) or ("Cape Town" in entry.description):
+                logger.info(f"Posting {entry.guid} - {entry.title}")
+                payload = entry.title + "\n\n" + strip_tags(entry.description)
+                try:
+                    length = 300
+                    while len(payload) > 249:
+                        payload = entry.title + "\n\n" + strip_tags(entry.description)
+                    logger.info(f"Posting tweet with body length: " + str(len(payload)))
+                    logger.info(f"Posting tweet with body: " + payload + "... " + entry.link)                
+                    post_to_twitter(payload, entry)
+                    post_to_slack(payload, entry)
+                    posts_table.put_item(
+                        Item={"guid": entry.guid, "title": entry.title, "link": entry.link}
+                    )
+                except Exception:
+                    logger.exception(f"Failed to post")
